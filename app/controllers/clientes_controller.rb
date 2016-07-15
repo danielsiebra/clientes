2427baa94d627ab0c00935b822b91cdf735fb806
Mirian Araujo <mirianaraujo589@gmail.com>
2016-07-15 14:40:49 -0300

    mudanças no controller(verif.), na db (pw) e no model (ativo)

--- app/controllers/clientes_controller.rb
+++ app/controllers/clientes_controller.rb
@@ -8,16 +8,21 @@ class ClientesController < ApplicationController
 		arquivo = params[:clientes][:nome_arquivo].tempfile
 		upload = CSV.table(arquivo, :col_sep => ';', :encoding => 'UTF-8')
 
-		upload.each do |row|
-			cli = Cliente.create(
-				nome: row.fetch(:nome),
-				telefone: row.fetch(:telefone),
-				endereco: row.fetch(:endereco),
-				cep: row.fetch(:cep),
-				valor: row.fetch(:valor),
-				vencimento: row.fetch(:vencimento)
-				)
+		if !row.fetch(:nome).nil? && !row.fetch(:telefone).nil? && !row.fetch(:endereco).nil? && !row.fetch(:cep).gsub(/\W\s?/,"").length == 8 && !row.fetch(:valor).nil? && !row.fetch(:vencimento).nil?
+
+			upload.each do |row|
+				cli = Cliente.create(
+					nome: row.fetch(:nome),
+					telefone: row.fetch(:telefone),
+					endereco: row.fetch(:endereco),
+					cep: row.fetch(:cep),
+					valor: row.fetch(:valor),
+					vencimento: row.fetch(:vencimento)
+					)
+			end
 		end
-		
 	end
+
+
+
 end
---
+++ db/migrate/20160715173909_add_ativo_to_clientes.rb
@@ -0,0 +1,4 @@
+class AddAtivoToClientes < ActiveRecord::Migration
+  def change
+  end
+end
